1b2e32b139a8b35239f5965a4fdfb3a415479311
asiob <agnes.siobud@gmail.com>
2022-09-14 14:53:43 -0400

    formulaire ajout produit

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -2,9 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Stock;
 use App\Entity\Produit;
+use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -19,9 +24,44 @@ class ProduitType extends AbstractType
             ->add('quantite')
             ->add('couleur')
             ->add('taille')
-            // ->add('categorie')
-            // ->add('stock')
-        
+            ->add('images'
+            , EntityType::class, [
+                'required' => true,
+                'label' => 'Ajouter une image',
+                'class' => Image::class,
+                'choice_label' => 'nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez ajouter une image'
+
+                        ])
+                ]
+            ])
+            ->add('categorie', EntityType::class, [
+                'required' => true,
+                'label' => 'choisir une catégorie',
+                'class' => Categorie::class,
+                'choice_label' => 'nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez choisir une catégorie'
+
+                        ])
+                ]
+            ])
+            ->add('stock', EntityType::class, [
+                'required' => true,
+                'label' => 'Entrer le stock',
+                'class' => Stock::class,
+                'choice_label' => 'quantite',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer le stock pour ce produit'
+
+                        ])
+                ]
+            ])
+        // le bouton
         ->add('save', SubmitType::class, [
             'label' => 'Ajouter un nouveau produit'
         ]
