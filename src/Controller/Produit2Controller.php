754210806fbd55b22e5981c31c49ec8f3bb2ec65
asiob <agnes.siobud@gmail.com>
2022-09-16 12:51:53 -0400

    formulaire image suite

---
+++ src/Controller/Produit2Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+/* namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produit', name: 'app_produit')]
+    public function index(ProduitRepository $produitRepository): Response
+    {
+        $produits = $produitRepository->findAll();
+
+        return $this->render('produit/index.html.twig', [
+            'lesProduits' => $produits,
+
+        ]);
+    }
+    
+    #[Route('/produit/new', name: 'new_produit')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $produit = new Produit();
+        $form = $this->createForm( ProduitType::class, $produit);
+
+        return $this->renderForm('produit/new.html.twig',[
+            'form' => $form,
+
+        ]);
+    }
+    
+} */
--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,38 +2,98 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
 use App\Entity\Produit;
 use App\Form\ProduitType;
+use App\Form\Produit1Type;
 use App\Repository\ProduitRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
+    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
     public function index(ProduitRepository $produitRepository): Response
     {
-        $produits = $produitRepository->findAll();
-
         return $this->render('produit/index.html.twig', [
-            'lesProduits' => $produits,
-
+            'produits' => $produitRepository->findAll(),
         ]);
     }
-    
-    #[Route('/produit/new', name: 'new_produit')]
-    public function new(Request $request, EntityManagerInterface $manager): Response
+
+    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProduitRepository $produitRepository): Response
     {
         $produit = new Produit();
-        $form = $this->createForm( ProduitType::class, $produit);
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
 
-        return $this->renderForm('produit/new.html.twig',[
+        if ($form->isSubmitted() && $form->isValid()) {
+            $couleur = $form->get('couleur')->getData();
+            // on recupere les images ajoutées
+            $images = $form->get('images')->getData();
+            // on boucle sur les images
+            foreach($images as $image){
+                // on génère un nouveau nom de fichier
+                $fichier = md5(uniqid()) . '.' . $images->guessExtension();
+                //on copie le fichier dans le dossier upload
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+                //on stocke l'image dans la base de donnée (son nom)
+                $img = new Image();
+                $img->setNom($fichier);
+                $produit->setCouleur($couleur);
+                $produit->addImage($img);
+            }
+
+            $produitRepository->add($produit, true);
+
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('produit/new.html.twig', [
+            'produit' => $produit,
             'form' => $form,
-
         ]);
     }
-    
+
+    #[Route('/{id}', name: 'app_produit_show', methods: ['GET'])]
+    public function show(Produit $produit): Response
+    {
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Produit $produit, ProduitRepository $produitRepository): Response
+    {
+        $form = $this->createForm(Produit1Type::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $produitRepository->add($produit, true);
+
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('produit/edit.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_produit_delete', methods: ['POST'])]
+    public function delete(Request $request, Produit $produit, ProduitRepository $produitRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$produit->getId(), $request->request->get('_token'))) {
+            $produitRepository->remove($produit, true);
+        }
+
+        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
