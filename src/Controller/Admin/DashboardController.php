c4a731e698e2d39879e8eec4e35446211a62144c
asiob <agnes.siobud@gmail.com>
2022-10-24 15:55:28 -0400

    erreur admin corrigee

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,20 +2,17 @@
 
 namespace App\Controller\Admin;
 
-
-use Symfony\Component\HttpFoundation\Response;
-use App\Controller\Admin\ProduitCrudController;
-use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    public function __construct(
-        private AdminUrlGenerator $adminUrlGenerator
-    ){
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator) {
+        
     }
 
     #[Route('/admin', name: 'admin')]
@@ -25,9 +22,8 @@ class DashboardController extends AbstractDashboardController
         $url = $this->adminUrlGenerator
             ->setController(ProduitCrudController::class)
             ->generateUrl();
-
-            return $this->redirect($url);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+//generer la route de l'affichage des produits
+        return $this->redirect($url);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -54,12 +50,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
-        yield MenuItem::linkToCrud('Produit', 'fas fa-map-marker-alt', Produit::class);
-        // yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class); rajouté par tuto
-
-
     }
 }
