41b51d26a096c8b538dbe9ba02b3ccb537425570
asiob <agnes.siobud@gmail.com>
2022-10-25 15:31:29 -0400

    erreur sur image string

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Produit;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProduitCrudController extends AbstractCrudController
@@ -12,14 +17,19 @@ class ProduitCrudController extends AbstractCrudController
         return Produit::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('titre'),
             TextEditorField::new('description'),
+            MoneyField::new('prix')->setCurrency('EUR'),
+            ImageField::new('images')
+                ->setBasePath('uploads')
+                ->setUploadDir('public/uploads'),
         ];
+
     }
-    */
+    
 }
